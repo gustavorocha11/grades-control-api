e79394335724556d7dccde83c713c8a9a2280098
Gustavo Rocha <gustavorocha11@gmail.com>
2020-06-24 23:37:11 -0300

    tests

--- src/app.js
+++ src/app.js
@@ -1,9 +1,23 @@
-const express = require('express');
+import express from 'express';
 const app = express();
-const port = 3000;
+import path from 'path';
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
+app.get('/', function(req, res){
+    res.sendfile(`${__dirname}/html/index.html`);
 });
+
+app.get('/sobre', function(req, res){
+    res.send('Bem vindos a minha pagina sobre!');
+});
+
+app.get('/blog', function(req, res){
+    res.send('Bem vindos a minha pagina blog!');
+});
+
+app.get('/ola/:cargo/:nome/:cor', function(req, res){
+    res.send("<h1>Olá "+req.params.nome+"</h1>"+"<h2>Seu Cargo é: "+req.params.cargo+"</h2>"+"<h3>Sua cor favorita é: "+req.params.cor);
+});
+
+
+app.listen(8081, function(){console.log("Servidor Rodando");});
